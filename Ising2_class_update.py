f31344775183d6f521460e9cd72132188e6995e0
CianReeves <40773589+CianReeves@users.noreply.github.com>
2018-11-10 21:18:31 +0000

    Add files via upload

--- Ising2_class_update.py
+++ Ising2_class_update.py
@@ -4,8 +4,14 @@ from scipy import constants
 import matplotlib.cm as cm
 from matplotlib.animation import FuncAnimation
 from numpy import random
-random.seed(69)
-
+#random.seed(69)
+N=50 #Number of different temperature points taken for certain results
+T=1.0#Initial temperature
+iterations=300 #number of iterations after the lattice is warmed up
+Temp=np.zeros(N) 
+Mag_avg=np.zeros(N) #List to store average magnetization for certain temps
+Mag_avg_sqr=np.zeros(N) #Stores second moment of magnetization
+suscept=np.zeros(N) #stores susceptibility
 class Ising:
     def __init__(self,size,warm_up,h,T):
         self.size = size
@@ -16,19 +22,20 @@ class Ising:
         self.T=T*self.J/self.K
         self.B=1.0/(self.T*self.K)
         self.h=h
+        
     
-    def Neighbours(self,s,i,j):
+    def Neighbours(self,s,i,j): #Nearest Neighbours for square lattice
         R=s[(i+1)%self.size][j]
         L=s[(i-1)%self.size][j]
         U=s[i][(j+1)%self.size]
         D=s[i][(j-1)%self.size]
         return(R+L+U+D)
     
-    def dE(self,i,j):
+    def dE(self,i,j): #Calclates del_E
         Eng=self.s[i][j]*self.Neighbours(self.s,i,j)
         return -2.0*Eng*self.J +2*self.h*self.s[i][j]
     
-    def Flip(self,s):
+    def Flip(self,s): #Runs through lattice and flips each particles spin and decides whether or not to accept
         for i in range (self.size):
             for j in range(self.size):
                 s[i][j]=-s[i][j]#
@@ -44,7 +51,7 @@ class Ising:
                     s[i][j]=s[i][j]
         return s
    
-    def Mag(self,s_warm):
+    def Mag(self,s_warm): #Calclates the magnetization
         Mag=0
         
         for i in range (self.size):
@@ -54,14 +61,18 @@ class Ising:
         Mag=1.0/(self.size**2)*Mag
         
         return Mag
-''' def Mag_sqr(self,s_warm):
-        Mag=0
-        for i in range (self.size):
-            for j in range(self.size):
-                Mag+=(s_warm[i][j])
-        Mag=1.0/(self.size**2)*Mag
+
+    def susceptibility(self,s_warm,j): #Calculates susceptibility at a certain temperature
+            for i in range(iterations): #Calculates magnetization for a series of warmed up lattices and then averages them
+                Mag_avg[j]+=self.Mag(s_warm)
+                Mag_avg_sqr[j]+=(self.Mag(s_warm))**2
+                s_warm=self.Flip(s_warm)
+            Mag_avg[j] = abs(Mag_avg[j])/iterations
+            Mag_avg_sqr[j]=Mag_avg_sqr[j]/iterations
+            suscept[j]=(1.0/self.T)*(Mag_avg_sqr[j]-Mag_avg[j]**2)
+            return suscept
         
-        return Mag
+''' 
         
     def Energy(self,s_warm):
         E_eq=0
@@ -69,46 +80,25 @@ class Ising:
             for j in range(size):
                 E_eq+=s_warm[i][j]*self.Neighbours(s_warm,i,j)
         E_eq=E_eq/2'''
-
-
-N=50
-T=1.0
-Mag_avg=np.zeros(N)
-Mag_avg_sqr=np.zeros(N)
-Temp=np.zeros(N)
-susceptibility=np.zeros(N)
-iterations=300
-for j in range(N):
-    latt=Ising(10,100,0,T)
-
-
-    '''f1=plt.figure()
-    ax=f1.add_subplot(131)
-    im=plt.imshow(latt.s,vmin="-1",vmax="1",cmap="plasma")'''               
     
+
+
+'''f1=plt.figure()
+    ax=f1.add_subplot(131)
+    im=plt.imshow(latt.s,vmin="-1",vmax="1",cmap="plasma")'''   
+for j in range(N):
+    latt=Ising(10,100,0,T) #Initializes Ising class with different temperatures
     for i in range(100): #Warms up Lattice
         s_warm=latt.Flip(latt.s)
-
-    #ax=f1.add_subplot(132)
-    #im=plt.imshow(s_warm,vmin="-1",vmax="1",cmap="plasma")
-
-    for i in range(iterations): #Calculates magnetization for a series of warmed up lattices and then averages them
-        Mag_avg[j]+=latt.Mag(s_warm)
-        Mag_avg_sqr[j]+=(latt.Mag(s_warm))**2
-        s_warm=latt.Flip(s_warm)
-        
-    
-    Mag_avg[j] = abs(Mag_avg[j])/iterations
-    Mag_avg_sqr[j]=Mag_avg_sqr[j]/iterations
+    susceptibility=latt.susceptibility(s_warm,j)
     Temp[j]=T
-    susceptibility[j]=(1.0/T)*(Mag_avg_sqr[j]-Mag_avg[j]**2)
     T+=.05
     
-f3=plt.figure()
+f3=plt.figure() #figure for average magnetization.
 ax3=f3.add_subplot(111)
 ax3.plot(Temp,Mag_avg,'ro')
 
-f4=plt.figure()
+f4=plt.figure()#Figure for susceptibility
 ax4=f4.add_subplot(111)
 ax4.plot(Temp,susceptibility,'b*')
 plt.show()
